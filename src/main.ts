336ec7992e3af2015f4a30d452b500fbfe4bc837
Daniel <jdrt.1012@gmail.com>
2024-06-20 23:07:06 -0400

    nuevo cambios

--- src/main.ts
+++ src/main.ts
@@ -5,35 +5,40 @@ import { printInfo, printLogo } from './core/logger';
 
 import packageJson from '../package.json';
 
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+// import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
-//import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    {
-      transport: Transport.TCP,
-      options: {
-        host: 'localhost',
-        port: 3100,
-      },
-    },
-  );
+  // const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+  //   AppModule,
+  //   {
+  //     transport: Transport.NATS,
+  //     options: {
+  //       servers: [process.env.NATS_SERVER || 'nats://localhost:4222'],
+  //     },
+  //   },
+  // );
 
-  // const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule);
 
-  // const config = new DocumentBuilder()
-  //   .setTitle('Document rest')
-  //   .setDescription('Description')
-  //   .setVersion('1.0')
-  //   .build();
-  // const document = SwaggerModule.createDocument(app, config);
-  // SwaggerModule.setup('api/docs', app, document);
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Document rest')
+    .setDescription('Description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
-  await app.listen();
+  await app.listen(process.env.PORT || 3100);
 
   printLogo();
   printInfo({
